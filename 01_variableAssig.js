6f88c5fa63f9695ddb8512e4b2e36cd6eac434d5
sushantkhilare25 <sskhilare25@gmail.com>
2023-09-20 16:19:58 +0530

    variable assignment
    bacis information

---
+++ 01_variableAssig.js
@@ -0,0 +1,22 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Sushant Khilare";
+console.log(myFullName);
+
+var myFullName = "Sushant Sunil Khilare";
+console.log(myFullName);
+
+var myLove = "l love only JS";
+console.log(myLove);
+
+
+var firstName = "Sushant";
+var lastName  = "Khilare";
+var age = '24';
+var address = "A/p Shirala";
+var pin_code ='415 408';
+var gender = "Male";
+var isMarried = "No Married";
+
+console.log('my firstname is:',firstName ,'my lastname is:',lastName,'my age is:',age,'my address is:',address,'my pin_code is:',pin_code,'my gender is:',gender,'my marriage status:',isMarried);
